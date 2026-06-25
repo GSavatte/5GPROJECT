25111fef788ec25672785090d7ce06ffff06d50e
GSavatte <gabrielbabou1@gmail.com>
2026-06-25 13:07:53 +0200

    Added another slice to the load test

--- load_test/seed.js
+++ load_test/seed.js
@@ -4,11 +4,14 @@ const slice_sd= "000001";
 
 print(`Lancement de l'injection de ${nombre_ues} UEs dans la base de données...`);
 
-for (let i = 1; i <= nombre_ues; i++) {
+for (let i = 1; i <= 2*nombre_ues; i+=2) {
   const imsiSuffix = i.toString().padStart(10, "0");
+  const imsiSuffix2 = (i+1).toString().padStart(10, "0");
   const imsiVal = "20801" + imsiSuffix;
-    const posX = 48.117883 + ((Math.random() - 0.5) * 0.01);
-    const posY = -1.640991 + ((Math.random() - 0.5) * 0.02);
+  const imsiVal2 = "20801" + imsiSuffix2;
+  const posX = 48.117883 + ((Math.random() - 0.5) * 0.01);
+  const posY = -1.640991 + ((Math.random() - 0.5) * 0.02);
+
 
   db.subscribers.insertOne({
     "alias": `Test Subscriber ${i} URLLC`,
@@ -34,5 +37,31 @@ for (let i = 1; i <= nombre_ues; i++) {
     "security": { "k": "00000000000000000000000000000000", "amf": "8000", "op": null, "opc": "00000000000000000000000000000000", "sqn": NumberLong(0) },
     "schema_version": 1
     })
+
+  db.subscribers.insertOne({
+    "alias": `Test Subscriber ${i+1} URLLC`,
+    "position": { "latitude": posX, "longitude": posY },
+    "imsi": imsiVal2,
+    "subscribed_rau_tau_timer": 12,
+    "network_access_mode": 0,
+    "subscriber_status": 0,
+    "access_restriction_data": 32,
+    "slice": [{
+        "sst": 2,
+        "sd": "000001",
+        "default_indicator": true,
+        "session": [{
+            "name": "internet",
+            "type": 3,
+            "pcc_rule": [],
+            "ambr": { "uplink": { "value": 1, "unit": 3 }, "downlink": { "value": 1, "unit": 3 } },
+            "qos": { "index": 9, "arp": { "priority_level": 8, "pre_emption_capability": 1, "pre_emption_vulnerability": 1 } }
+        }]
+    }],
+    "ambr": { "uplink": { "value": 1, "unit": 3 }, "downlink": { "value": 1, "unit": 3 } },
+    "security": { "k": "00000000000000000000000000000000", "amf": "8000", "op": null, "opc": "00000000000000000000000000000000", "sqn": NumberLong(0) },
+    "schema_version": 1
+    })
 }
+
 print(`✅ ${nombre_ues} abonnés préparés avec succès !`);
--- load_test/start_load_test.sh
+++ load_test/start_load_test.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-
 echo "Installation de curl..."
-sleep 5
+sleep 0.5
 apt-get update -y > /dev/null 2>&1
 apt-get install curl -y > /dev/null 2>&1
 echo "Installation de curl terminée."
 
-nombre_ues=200
+nombre_ues=400
 NUM_GNBS=4
 
 echo "Base de données prête. Résolution des antennes..."
@@ -30,19 +29,29 @@ done
 
 echo "Démarrage de la simulation..."
 
-for i in $(seq 1 $nombre_ues); do
+for i in $(seq 1 2 $nombre_ues); do
   SUFFIX=$(printf "%010d" $i)
+  SUFFIX2=$(printf "%010d" $((i+1)))
   IMSI="imsi-20801$SUFFIX"
+  IMSI2="imsi-20801$SUFFIX2"
   
   GNB_ID=$(( ((i - 1) % NUM_GNBS) + 1 ))
+  GNB_ID2=$(( ((i) % NUM_GNBS) + 1 ))
   GNB_TARGET=${GNB_IPS[$GNB_ID]} 
+  GNB_TARGET2=${GNB_IPS[$GNB_ID2]}
   
   sed -e "s/IMSI_PLACEHOLDER/$IMSI/g" -e "s/GNB_PLACEHOLDER/$GNB_TARGET/g" /config/ue-template.yaml > "/tmp/ue-${IMSI}.yaml"
+
+  sed -e "s/IMSI_PLACEHOLDER/$IMSI2/g" -e "s/GNB_PLACEHOLDER/$GNB_TARGET2/g" /config/ue-template2.yaml > "/tmp/ue-${IMSI2}.yaml"
   
   /UERANSIM/nr-ue -c "/tmp/ue-${IMSI}.yaml" > "/tmp/logs-${IMSI}.txt" 2>&1 &
+  /UERANSIM/nr-ue -c "/tmp/ue-${IMSI2}.yaml" > "/tmp/logs-${IMSI2}.txt" 2>&1 &
   
   sleep 0.1 
 done
 
-echo "Simulation lancée."
-tail -f /dev/null
+echo "Toutes les requêtes d'attachement on été envoyées aux gNBs. En attente de la création des interfaces réseau pour les UEs..."
+
+sleep 5
+
+bash -c "bash /config/generate_traffic.sh"
