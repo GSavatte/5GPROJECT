6b3cbd21203b5eae7bc1b6776f44b3d2c1fbd258
GSavatte <gabrielbabou1@gmail.com>
2026-06-29 16:32:24 +0200

    Added user-friendly scripts to start and stop the network and his components

--- utils/scripts/start.sh
+++ utils/scripts/start.sh
@@ -1,25 +1,40 @@
 #!/bin/bash
 
+
 echo "Démarrage du cœur de réseau..."
-docker compose up -d amf ausf bsf nrf nssf pcf smf1 smf2 smf3 upf1 upf2 upf3 udm udr
+docker compose --env-file=.env -f compose-files/docker-compose.yml up -d amf ausf bsf db nrf nssf pcf smf1 smf2 smf3 upf1 upf2 upf3 udm udr
 sleep 5
+
+sudo bash utils/scripts/clear.sh
+sudo bash utils/scripts/start_gnbs.sh
 
 echo "Démarrage des antennes (gNBs)..."
-docker compose up -d gnb1 gnb2 gnb3 gnb4
+
+docker compose -f compose-files/docker-compose.gnbs.yaml up -d
 sleep 5
 
-echo "Démarrage progressif des UEs..."
-UE_CONTAINERS=("ue1" "ue2" "ue3" "ue4" "ue5" "ue6" "ue7" "ue8" "ue9" "ue10")
+echo "Réseau 5G déployé avec succès."
 
-for ue in "${UE_CONTAINERS[@]}"; do
-    echo "  -> Allumage de $ue"
-    docker compose up -d "$ue"
-    sleep 2 
-done
-
-if [ "$1" == "--webui" ]; then
+read -p "Voulez-vous lancer l'interface web permettant de gérer les subscribers, accéder à la carte du réseau etc... ? (y/n) " launch
+if [[ "$launch" == "y" || "$launch" == "Y" ]]; then
     echo "Lancement de l'interface web..."
-    docker compose up -d webui
+    docker compose --env-file=.env -f compose-files/docker-compose.yml up -d webui
+    sleep 2
+    echo "Interface web lancée. Accédez-y via http://localhost:9999"
 fi
 
-echo "Réseau entièrement déployé."
+read -p "Voulez-vous lancer le monitoring du réseau 5G ? (y/n) " monitor
+if [[ "$monitor" == "y" || "$monitor" == "Y" ]]; then
+    echo "Lancement du monitoring..."
+    docker compose --env-file=.env -f compose-files/docker-compose.monitoring.yaml up -d 
+    sleep 2
+    echo "Monitoring lancé. Accédez-y via http://localhost:3000 (login: admin, password: admin)"
+fi
+
+read -p "Voulez-vous lancer un test de charge avec 400 UEs et générer du trafic vers le conteneur internet-sim ? (y/n) " loadtest
+if [[ "$loadtest" == "y" || "$loadtest" == "Y" ]]; then
+    echo "Lancement du test de charge..."
+    docker compose --env-file=.env -f compose-files/docker-compose.loadtest.yaml up -d
+    sleep 2
+    echo "Test de charge lancé. Les logs des UEs sont disponibles dans le conteneur loadtest."
+fi
---
+++ utils/scripts/stop.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Arrêt complet du réseau 5G et de tous les conteneurs associés..."
+docker compose -f compose-files/docker-compose.gnbs.yaml down
+docker compose -f compose-files/docker-compose.loadtest.yaml down
+docker compose -f compose-files/docker-compose.monitoring.yaml down
+sudo bash utils/scripts/clear.sh
+docker compose -f compose-files/docker-compose.yml down
+
+echo "Tous les conteneurs associés au réseau ont été stoppés."
+
