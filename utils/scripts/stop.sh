3551b853539936e39766ba2db170d19d6748c7ce
GSavatte <gabrielbabou1@gmail.com>
2026-07-20 11:31:03 +0200

    Fixed stop script

--- utils/scripts/stop.sh
+++ utils/scripts/stop.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
 echo "Arrêt complet du réseau 5G et de tous les conteneurs associés..."
+
+docker stop ue-loadtester >/dev/null 2>&1
+docker rm -f ue-loadtester >/dev/null 2>&1
+
 docker compose --env-file=.env -f compose-files/docker-compose.gnbs.yaml down
 docker compose --env-file=.env -f compose-files/docker-compose.loadtest.yaml down
 docker compose --env-file=.env -f compose-files/docker-compose.monitoring.yaml down
 sudo bash utils/scripts/clear.sh
-docker compose --env-file=.env -f compose-files/docker-compose.yml down
+docker compose --env-file=.env -f compose-files/docker-compose.yml down --remove-orphans
 
 echo "Tous les conteneurs associés au réseau ont été stoppés."
 
